5ae9795eb0a0296d4c57b9832a8624c9a14a75bd
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-04-22 16:12:54 +0200

    ht-test

---
+++ ht-test/sources/HtTest.java
@@ -0,0 +1,73 @@
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadMXBean;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.String.format;
+
+class HTTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length == 0) {
+            System.err.println("Usage: httest <opcount> <numthreads>");
+            System.exit(1);
+        }
+
+        long opsCount = parseLong(args[0]);
+        int numThreads = parseInt((args[1]));
+
+        var executor = Executors.newFixedThreadPool(numThreads);
+
+        long finalResult = 0;
+        final Map<Integer, Long> resultsPerThread = new ConcurrentHashMap<>();
+        final Map<Integer, Long> cpuTimePerThread = new ConcurrentHashMap<>();
+        final Map<Integer, Long> wallTimePerThread = new ConcurrentHashMap<>();
+
+        var totalWallTimeBefore = System.currentTimeMillis();
+        CountDownLatch latch = new CountDownLatch(numThreads);
+        for (int i = 0; i < numThreads; ++i) {
+            final int threadId = i;
+            executor.submit(() -> {
+                ThreadMXBean threadMXBean = ManagementFactory.getThreadMXBean();
+                var wallTimeBefore = System.currentTimeMillis();
+                var cpuTimeBefore = threadMXBean.getCurrentThreadCpuTime();
+                long result = 0;
+                for(long j = 0; j < opsCount; j++) {
+                    result = (result + j) * j;
+                }
+                var cpuTimeAfter = threadMXBean.getCurrentThreadCpuTime();
+                var wallTimeAfter = System.currentTimeMillis();
+                resultsPerThread.put(threadId, result);
+                cpuTimePerThread.put(threadId, (cpuTimeAfter - cpuTimeBefore)/1000000);
+                wallTimePerThread.put(threadId, wallTimeAfter - wallTimeBefore);
+                latch.countDown();
+            });
+        }
+
+        latch.await();
+
+        var totalWallTimeAfter = System.currentTimeMillis();
+        var totalWallTime = totalWallTimeAfter - totalWallTimeBefore;
+        for (Long threadResult : resultsPerThread.values()) {
+            finalResult = finalResult + threadResult;
+        }
+        System.out.println("finalResult = " + finalResult);
+        System.out.println();
+        long sumCpuTime = 0;
+        long sumWallTime = 0;
+        for (int i = 0; i < numThreads; i++) {
+            System.out.println(format("[%s] wall=%s; cpu=%s", i, wallTimePerThread.get(i), cpuTimePerThread.get(i)));
+            sumCpuTime += cpuTimePerThread.get(i);
+            sumWallTime += wallTimePerThread.get(i);
+        }
+        System.out.println("---------------");
+        System.out.println(format("totalWallTime=%s; sumWallTime=%s; sumCpuTime=%s",
+                totalWallTime, sumWallTime, sumCpuTime));
+
+        executor.shutdownNow();
+    }
+}
